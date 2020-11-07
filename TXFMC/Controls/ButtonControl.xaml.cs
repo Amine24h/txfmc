24630eba333f8ba374f449a5ad5b7ebedba38283
Amine KABOUCHE <amine.caius@gmail.com>
2020-11-07 22:29:28 +0100

    Add Custom Button Control

---
+++ TXFMC/Controls/ButtonControl.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TXFMC.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonControl : ContentView
+    {
+        public ButtonControl()
+        {
+            InitializeComponent();
+        }
+
+        BindableProperty ImageProperty = BindableProperty.Create(nameof(Image),
+                                                                 typeof(ImageSource),
+                                                                 typeof(ButtonControl),
+                                                                 default(ImageSource),
+                                                                 BindingMode.TwoWay);
+
+        BindableProperty TextProperty = BindableProperty.Create(nameof(Text),
+                                                                 typeof(string),
+                                                                 typeof(ButtonControl),
+                                                                 default(string),
+                                                                 BindingMode.TwoWay);
+
+        BindableProperty BtnBackgroundColorProperty = BindableProperty.Create(nameof(BtnBackgroundColor),
+                                                                 typeof(Color),
+                                                                 typeof(ButtonControl),
+                                                                 default(Color),
+                                                                 BindingMode.TwoWay);
+
+        BindableProperty TextColorProperty = BindableProperty.Create(nameof(TextColor),
+                                                                 typeof(Color),
+                                                                 typeof(ButtonControl),
+                                                                 default(Color),
+                                                                 BindingMode.TwoWay);
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public Color BtnBackgroundColor
+        {
+            get { return GetValue(BtnBackgroundColorProperty) != null ? (Color)GetValue(BtnBackgroundColorProperty) : Color.Transparent; }
+            set { SetValue(BtnBackgroundColorProperty, value); }
+        }
+
+        public Color TextColor
+        {
+            get { return GetValue(TextColorProperty) != null ? (Color)GetValue(TextColorProperty) : Color.Black; }
+            set { SetValue(TextColorProperty, value); }
+        }
+    }
+}
